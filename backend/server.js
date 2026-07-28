2203e5f96afbefba2e93159164cd82b4372ded7c
Ali Mohamed <01225280445alimoham@gmail.com>
2026-07-28 18:01:26 +0300

    Build the strucrtre of the backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv";
+
+//App config
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middleware
+app.use(express.json);
+app.use(cors);
+
+// endpoints
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log("server started on port " + port);
+});
